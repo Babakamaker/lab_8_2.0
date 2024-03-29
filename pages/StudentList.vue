39ba62ad8c8f3e006aaaec98f5368c946ba60e66
Viktoriia Babaieva <viktoriia.babaieva@oa.edu.ua>
2024-03-30 01:19:17 +0200

    feed: data from API was fetched, table was created and filled

--- pages/StudentList.vue
+++ pages/StudentList.vue
@@ -1,93 +1,53 @@
 <script setup lang="ts">
-const columns = [{
-  key: 'id',
-  label: 'ID'
-}, {
-  key: 'name',
-  label: 'Name'
-}, {
-  key: 'title',
-  label: 'Title'
-}, {
-  key: 'email',
-  label: 'Email'
-}, {
-  key: 'role',
-  label: 'Role'
-}]
 
-const people = [{
-  id: 1,
-  name: 'Lindsay Walton',
-  title: 'Front-end Developer',
-  email: 'lindsay.walton@example.com',
-  role: 'Member'
-}, {
-  id: 2,
-  name: 'Courtney Henry',
-  title: 'Designer',
-  email: 'courtney.henry@example.com',
-  role: 'Admin'
-}, {
-  id: 3,
-  name: 'Tom Cook',
-  title: 'Director of Product',
-  email: 'tom.cook@example.com',
-  role: 'Member'
-}, {
-  id: 4,
-  name: 'Whitney Francis',
-  title: 'Copywriter',
-  email: 'whitney.francis@example.com',
-  role: 'Admin'
-}, {
-  id: 5,
-  name: 'Leonard Krasner',
-  title: 'Senior Designer',
-  email: 'leonard.krasner@example.com',
-  role: 'Owner'
-}, {
-  id: 6,
-  name: 'Floyd Miles',
-  title: 'Principal Designer',
-  email: 'floyd.miles@example.com',
-  role: 'Member'
-}]
+const columns = [
+  { key: 'title', label: 'Назва' },
+  { key: 'description', label: 'Опис' },
+  { key: 'price', label: 'Ціна' },
+  { key: 'rating', label: 'Оцінка' },
+  { key: 'brand', label: 'Бренд' },
+  { key: 'category', label: 'Категорія' },
+  { key: 'thumbnail', label: 'Фото' }
+];
+const {data, pending} = await useLazyAsyncData<any>(
+    'products', () => $fetch('https://dummyjson.com/products'))
+
+let products = data.value.products;
+
+const page = ref(2)
+const pageCount = 4
+
 
 const q = ref('')
-
-const filteredRows = computed(() => {
+const rows = computed(() => {
   if (!q.value) {
-    return people
+    return products.slice((page.value - 1) * pageCount, (page.value) * pageCount)
+
   }
 
-  return people.filter((person) => {
-    return Object.values(person).some((value) => {
+  return products.filter((product: any) => {
+    return Object.values(product).some((value) => {
       return String(value).toLowerCase().includes(q.value.toLowerCase())
+
+
     })
   })
 })
-const page = ref(1)
-const pageCount = 5
 
-const rows = computed(() => {
-  const startIndex = (page.value - 1) * pageCount
-  return filteredRows.value.slice(startIndex, startIndex + pageCount)
-})
-
-const totalFilteredPeople = computed(() => {
-  return filteredRows.value.length
-})
 </script>
 
 <template>
-  <div>
-    <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
-      <UInput v-model="q" placeholder="Filter people..." />
+  <div class="">
+    <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700 ">
+      <UInput v-model="q" placeholder="Filter products..." />
     </div>
 
-    <UTable :rows="rows" :columns="columns" />
-    <UPagination v-model="page" :page-count="pageCount" :total="totalFilteredPeople" />
+    <UTable :rows="rows" :columns="columns" >
+
+      </UTable>
+
+
+    <UPagination v-model="page" :page-count="pageCount" :total="products.length" />
   </div>
 </template>
 
